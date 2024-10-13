925e89997a34b255c0140d43970fe47e886777e2
Julian Canepa <julian.l.canepa@gmail.com>
2024-10-13 16:22:48 -0600

    Complete aes-128-ecb decryption/encryption.

--- openssl-command.sh
+++ openssl-command.sh
@@ -1,23 +1,23 @@
 #! /usr/bin/env bash
 
 # Define variables
-CIPHER="-bf-ecb" # -aes-128-cfb, bf-cbc
-KEY="a3a02c24f886d7a6dba8633cb9fd08c2"
-IV="15d82a74fe05c7919368040e5c6c27fc"
-SALT="1d0f212b0d021b4b8e30ddcd5fc3d3a1"
-PASS="sT8k"
-P_FILE=""
-E_FILE="newer_bf_ecb.enc"
+CIPHER="-aes-128-ecb"
+KEY="6FAE6A22F99127D79B21D7A0F1949D14"
+IV=""
+SALT="1C578762DA631007"
+PASS=""
+P_FILE="plaintext.txt"
+E_FILE="aes-128-ecb.enc"
 D_FILE="decrypted.txt"
 
 # display the plaintext message
-# cat $P_FILE
+cat $P_FILE
 
 # display the plain text in hex
 # xxd -ps $P_FILE
 
 # encrypt the plaintext file
-# openssl enc $CIPHER -e -in $P_FILE -out $E_FILE -K $KEY ${IV:+-iv $IV} ${SALT:+-S $SALT}
+openssl enc $CIPHER -e -in $P_FILE -out $E_FILE -K $KEY ${IV:+-iv $IV} ${SALT:+-S $SALT} ${PASS:+-pass pass:$PASS}
 
 # get the hex values to compare
 # xxd -ps $E_FILE
@@ -26,7 +26,7 @@ D_FILE="decrypted.txt"
 openssl enc $CIPHER -d -in $E_FILE -out $D_FILE -K $KEY ${IV:+-iv $IV} ${SALT:+-S $SALT} ${PASS:+-pass pass:$PASS}
 
 # display the decrypted file
-# cat ./$D_FILE
+cat ./$D_FILE
 
 # clean up
 # rm ./$E_FILE

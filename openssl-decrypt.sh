2a9e617b816ef86aec4589587f38e59456bfc9a9
Julian Canepa <julian.l.canepa@gmail.com>
2024-10-13 17:03:14 -0600

    Attempting camilla and blowfish to no avail.

--- openssl-decrypt.sh
+++ openssl-decrypt.sh
@@ -1,16 +1,22 @@
 #! /usr/bin/env bash
 
 # Define variables
-CIPHER="-bf-cbc"
-KEY="AACEA9897DE38A386F744C35451CED29"
-IV="57FE234E6033869E"
-SALT="67C4437621DEDAC1"
+CIPHER="-camellia-128-ecb"
+KEY="100F15BBF72702AA37153F406F16E0FA"
+IV=""
+SALT="810C6E944A0B5E81"
 PASS=""
-E_FILE="encryption-protocols/ciphertext_bf/ciphertext_bf-cbc.enc"
+E_FILE="encryption-protocols/ciphertext_camellia/ciphertext_camellia-128-ecb.enc"
 D_FILE="decrypted.txt"
 
 # decrypt the ciphertext file
-openssl enc $CIPHER -d -in $E_FILE -out $D_FILE -K $KEY ${IV:+-iv $IV} ${SALT:+-S $SALT} ${PASS:+-pass pass:$PASS}
+openssl enc $CIPHER -d -in $E_FILE -out $D_FILE -K $KEY ${IV:+-iv $IV} ${SALT:+-S $SALT} ${PASS:+-pass pass:$PASS} -provider legacy -provider default
 
-# display the decrypted file
-cat ./$D_FILE
+# Check the file type
+if file decrypted.txt | grep -q "image"; then
+    echo "This is an image file"
+    mv decrypted.txt decrypted.png
+    cp decrypted.png decrypted.jpg
+else
+    echo "Unknown file type"
+fi

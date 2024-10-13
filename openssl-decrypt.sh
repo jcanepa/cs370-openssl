f05577ec62990e8a0aebb0c7a0ec735868473034
Julian Canepa <julian.l.canepa@gmail.com>
2024-10-13 17:37:30 -0600

    Working on DES.

--- openssl-decrypt.sh
+++ openssl-decrypt.sh
@@ -1,22 +1,15 @@
 #! /usr/bin/env bash
 
 # Define variables
-CIPHER="-camellia-128-ecb"
-KEY="100F15BBF72702AA37153F406F16E0FA"
+CIPHER="-des-ecb"
+KEY="2EB034E7B3F6C5A5"
 IV=""
-SALT="810C6E944A0B5E81"
+SALT="A3F4D4A349C6E3DC"
 PASS=""
-E_FILE="encryption-protocols/ciphertext_camellia/ciphertext_camellia-128-ecb.enc"
-D_FILE="decrypted.txt"
+E_FILE="encryption-protocols/ciphertext_des/ciphertext_des-ecb.enc"
+D_FILE="decrypted${CIPHER}.txt"
 
 # decrypt the ciphertext file
 openssl enc $CIPHER -d -in $E_FILE -out $D_FILE -K $KEY ${IV:+-iv $IV} ${SALT:+-S $SALT} ${PASS:+-pass pass:$PASS} -provider legacy -provider default
 
-# Check the file type
-if file decrypted.txt | grep -q "image"; then
-    echo "This is an image file"
-    mv decrypted.txt decrypted.png
-    cp decrypted.png decrypted.jpg
-else
-    echo "Unknown file type"
-fi
+cat $D_FILE ; echo

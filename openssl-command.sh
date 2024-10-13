aaf7db5cafdee8a2859e83199d57fd70bddf921f
Julian Canepa <julian.l.canepa@gmail.com>
2024-10-13 15:44:31 -0600

    Add another student encrytion file to decrypt.

--- openssl-command.sh
+++ openssl-command.sh
@@ -1,26 +1,26 @@
 #! /usr/bin/env bash
 
 # Define variables
-CIPHER="-aes-128-ecb" # -aes-128-cfb, bf-cbc
+CIPHER="-bf-ecb" # -aes-128-cfb, bf-cbc
 KEY="c705fe21632c4031808148d9bbe069f9"
 IV="" # not used for this cipher
 SALT="" # not used for this cipher
 P_FILE="plaintext.txt"
-E_FILE="ciphertext.enc"
+E_FILE="newer_bf_ecb.enc"
 D_FILE="decrypted.txt"
 
 
 # display the plaintext message
-cat ./$P_FILE
+# cat $P_FILE
 
 # display the plain text in hex
-xxd -ps $P_FILE
+# xxd -ps $P_FILE
 
 # encrypt the plaintext file
-openssl enc $CIPHER -e -in $P_FILE -out $E_FILE -K $KEY ${IV:+-iv $IV} ${SALT:+-S $SALT}
+# openssl enc $CIPHER -e -in $P_FILE -out $E_FILE -K $KEY ${IV:+-iv $IV} ${SALT:+-S $SALT}
 
 # get the hex values to compare
-xxd -ps $E_FILE
+# xxd -ps $E_FILE
 
 # decrypt the ciphertext file
 openssl enc $CIPHER -d -in $E_FILE -out $D_FILE -K $KEY ${IV:+-iv $IV} ${SALT:+-S $SALT}

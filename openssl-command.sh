4ed32e0ccb9217d3aac2ee526f4165812f765ff6
Julian Canepa <julian.l.canepa@gmail.com>
2024-10-12 20:14:04 -0600

    Initial commit.

---
+++ openssl-command.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/env bash
+
+# Define variables
+CIPHER="-aes-128-ecb" # -aes-128-cfb, bf-cbc
+KEY="c705fe21632c4031808148d9bbe069f9"
+IV="e71cdda0930b323e0000000000000000" # not used for this cipher
+P_FILE="plaintext.txt"
+E_FILE="ciphertext.enc"
+D_FILE="decrypted.txt"
+
+
+# display the plaintext message
+cat ./$P_FILE
+echo ""
+
+# display the plain text in hex
+xxd -ps $P_FILE
+
+# encrypt the plaintext file
+openssl enc $CIPHER -e -in $P_FILE -out $E_FILE -K $KEY
+
+# get the hex values to compare
+xxd -ps $E_FILE
+
+# decrypt the ciphertext file
+openssl enc $CIPHER -d -in $E_FILE -out $D_FILE -K $KEY
+
+# display the decrypted file
+cat ./$D_FILE
+
+# clean up
+rm ./$E_FILE
+rm ./$D_FILE

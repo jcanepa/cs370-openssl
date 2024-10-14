6c8412398f88db8cdce4efffc2c6e356c2b5d873
Julian Canepa <julian.l.canepa@gmail.com>
2024-10-13 18:02:06 -0600

    Testing cast.

--- openssl-decrypt.sh
+++ openssl-decrypt.sh
@@ -1,15 +1,15 @@
 #! /usr/bin/env bash
 
 # Define variables
-CIPHER="-des-ecb"
-KEY="2EB034E7B3F6C5A5"
-IV=""
-SALT="A3F4D4A349C6E3DC"
+CIPHER="-cast-cbc"
+KEY="854D81D270B8DAB970C18D47FC999C70"
+IV="B624DF51A17F7BD9"
+SALT="3B6514F5B2404FE0"
 PASS=""
-E_FILE="encryption-protocols/ciphertext_des/ciphertext_des-ecb.enc"
+E_FILE="encryption-protocols/ciphertext_cast/ciphertext_cast-cbc.enc"
 D_FILE="decrypted${CIPHER}.txt"
 
 # decrypt the ciphertext file
-openssl enc $CIPHER -d -in $E_FILE -out $D_FILE -K $KEY ${IV:+-iv $IV} ${SALT:+-S $SALT} ${PASS:+-pass pass:$PASS} -provider legacy -provider default
+openssl enc $CIPHER -d -in $E_FILE -out $D_FILE -K $KEY ${IV:+-iv $IV} ${PASS:+-pass pass:$PASS} -provider legacy
 
 cat $D_FILE ; echo

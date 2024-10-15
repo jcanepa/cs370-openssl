4a7584cbba17b9b60361d4486f26d466b8d4ace8
Julian Canepa <julian.l.canepa@gmail.com>
2024-10-14 23:38:23 -0600

    Complete cast.

--- cast/go.sh
+++ cast/go.sh
@@ -1,26 +1,26 @@
 #! /usr/bin/env bash
-CIPHER="-aes-256-cbc"
+
+CIPHER="-cast5-ofb"
 E_FILE="encrypted-test.txt"
 D_FILE="decrypted-test.txt"
 P_FILE="plain.txt"
 PASSWORD="password"
 
+# clean up
+rm decrypted-test.txt
+rm $E_FILE
 
 # encrypt the file
-openssl enc ${CIPHER} -salt -in ${P_FILE} -out ${E_FILE} -pass pass:${PASSWORD} -pbkdf2
+openssl enc ${CIPHER} -salt -in ${P_FILE} -out ${E_FILE} -pass pass:${PASSWORD} -pbkdf2 -provider legacy -provider default
 
 # reveal the secrets
 openssl enc ${CIPHER} -salt -in ${P_FILE} -pass pass:${PASSWORD} -pbkdf2 -provider legacy -provider default -P | sed 's/ //g'
 echo "password=${PASSWORD}"
 
 # decrypt the file
-openssl enc -d ${CIPHER} -in ${E_FILE} -out decrypted-test.txt -pass pass:${PASSWORD} -pbkdf2
+openssl enc -d ${CIPHER} -in ${E_FILE} -out decrypted-test.txt -pass pass:${PASSWORD} -pbkdf2 -provider legacy -provider default
 
 # compare
 cat ${P_FILE}
 cat decrypted-test.txt
 diff ${P_FILE} decrypted-test.txt
-
-# # clean up
-rm decrypted-test.txt
-rm $E_FILE

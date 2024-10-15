a7ad085c570b44e0fb47821c60a73fda2835b56b
Julian Canepa <julian.l.canepa@gmail.com>
2024-10-15 15:40:36 -0600

    Rename blowfish directory. Troubleshooting decryption.

--- _blowfish/decrypt/go.sh
+++ _blowfish/decrypt/go.sh
@@ -7,4 +7,4 @@ D_FILE="decrypted.txt"
 KEY="8123456789eBC7EFb123a563"
 IV="023a5631"
 
- openssl enc -d -${CIPHER} -in ${E_FILE} -out ${D_FILE} -provider legacy -provider default
+ echo "openssl enc -d -${CIPHER} -in ${E_FILE} -out ${D_FILE}"

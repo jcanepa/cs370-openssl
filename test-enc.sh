6144f2f1ca07cb5db2586af2e35f84d44fbed4c7
Julian Canepa <julian.l.canepa@gmail.com>
2024-10-14 23:04:57 -0600

    Tinkering...

--- test-enc.sh
+++ test-enc.sh
@@ -4,10 +4,15 @@
 CIPHER="-cast5-ofb"
 P_FILE="plain.txt"
 E_FILE="encrypted${CIPHER}.enc"
-PASSWORD="memento"
+PASSWORD="mementomori"
+CMD_OPTIONS="-provider legacy -provider default"
 
 # encrypt the file
-openssl enc $CIPHER -salt -in plain.txt -out encrypted-test.enc -pass pass:${PASSWORD} -pbkdf2 -provider legacy -provider default
+openssl enc $CIPHER -salt -in $P_FILE -out encrypted${CIPHER}.enc -pass pass:${PASSWORD} -pbkdf2 -provider legacy -provider default
+
+# manually extract the key & iv, then spill all secrets
+openssl enc -cast5-ofb -salt -in $P_FILE -pass pass:${PASSWORD} -pbkdf2 -provider legacy -provider default -P
+echo "password=${PASSWORD}"
 
 # decrypt the file
 # openssl enc -d -aes-256-cbc -in encrypted-test.enc -out decrypted-test.txt -pass pass:password -pbkdf2
